5283013467b558c4216e8d67363728fdb254694f
Eddy Chase <eddykanyz.ek@gmail.com>
2021-04-11 23:09:31 +0300

    create a neighbourhood function

--- hangout/views.py
+++ hangout/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from .forms import SignupForm, BusinessForm
+from .forms import *
 from .models import *
 
 # Create your views here.
@@ -32,3 +32,15 @@ def hoods(request):
         'all_hoods': all_hoods,
     }
     return render(request, 'all_hoods.html', params)
+
+def create_hood(request):
+    if request.method == 'POST':
+        form = NeighbourHoodForm(request.POST, request.FILES)
+        if form.is_valid():
+            hood = form.save(commit=False)
+            hood.admin = request.user.profile
+            hood.save()
+            return redirect('hood')
+    else:
+        form = NeighbourHoodForm()
+    return render(request, 'newhood.html', {'form': form})

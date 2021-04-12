d8b4edc22413d9e4ae179e6cbbb2d83c5dcfd66a
Eddy Chase <eddykanyz.ek@gmail.com>
2021-04-12 09:41:13 +0300

    make and delete business functions

--- hangout/models.py
+++ hangout/models.py
@@ -58,3 +58,13 @@ class Business(models.Model):
     def __str__(self):
         return f'{self.name} Business'
 
+    def create_business(self):
+        self.save()
+
+    def delete_business(self):
+        self.delete()
+
+    @classmethod
+    def search_business(cls, name):
+        return cls.objects.filter(name__icontains=name).all()
+
